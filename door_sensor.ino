a55f6ab46624bde98cd2dfcd3e02db181940c7b6
Laura Mun <laurasumun@gmail.com>
2017-07-03 10:58:21 -0400

    initial commit

---
+++ door_sensor.ino
@@ -0,0 +1,18 @@
+const int MAGNET = 5;
+const int RELAY = 22;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(MAGNET, INPUT_PULLUP);
+ pinMode(RELAY, OUTPUT);
+}
+
+void loop() {
+ int magnetState = digitalRead(MAGNET);
+ Serial.println(magnetState);
+ if(magnetState == LOW) {
+   digitalWrite(RELAY, LOW);
+ } else {
+   digitalWrite(RELAY, HIGH);
+ }
+}
